2c540bc875505da73754ca3ae20f32641ba8418d
erictherobot <erictherobot@gmail.com>
2020-04-07 09:29:40 -0400

    Adding initial files

---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<svelte:head>
+  <title>Aframe Project</title>
+</svelte:head>
+
+<a-scene>
+  <a-entity
+    id="rain"
+    particle-system="preset: rain; color: #24CAFF; particleCount: 5000" />
+
+  <a-entity
+    id="sphere"
+    geometry="primitive: sphere"
+    material="color: #EFEFEF; shader: flat"
+    position="0 0.15 -5"
+    light="type: point; intensity: 5"
+    animation="property: position; easing: easeInOutQuad; dir: alternate; dur:
+    1000; to: 0 -0.10 -5; loop: true" />
+
+  <a-entity
+    id="ocean"
+    ocean="density: 20; width: 50; depth: 50; speed: 4"
+    material="color: #9CE3F9; opacity: 0.75; metalness: 0; roughness: 1"
+    rotation="-90 0 0" />
+
+  <a-entity
+    id="sky"
+    geometry="primitive: sphere; radius: 5000"
+    material="shader: gradient; topColor: 235 235 245; bottomColor: 185 185 210"
+    scale="-1 1 1" />
+
+  <a-entity id="light" light="type: ambient; color: #888" />
+</a-scene>
